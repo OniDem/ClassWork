3b55f2a97d33a86be498deb7b92f87a114ef8704
OniDem1 <spaindecraft17@gmail.com>
2024-02-05 13:10:39 +0700

    Добавьте файлы проекта.

---
+++ ClassWork/Program.cs
@@ -0,0 +1,33 @@
+﻿interface IMeterDistance
+{
+    public int KilometerToMeters(KilometerDistance distance);
+}
+
+class KilometerDistance
+{
+    public int Distance { get; set; }
+
+    public KilometerDistance(int distance)
+    {
+        Distance = distance;
+    }
+}
+
+class MeterAdapter : IMeterDistance
+{
+    const int k = 1000;
+    public int KilometerToMeters(KilometerDistance distance)
+    {
+        return distance.Distance * k;
+    }
+}
+
+class ClassWork()
+{
+    static void Main()
+    {
+        KilometerDistance distance = new(5);
+        MeterAdapter adapter = new();
+        Console.WriteLine(adapter.KilometerToMeters(distance));
+    }
+}
